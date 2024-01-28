0d35054a09a706ae40e6f950bf9a6916d96a970a
Ferperemero86 <fernandoperezmerono@gmail.com>
2024-01-28 13:13:39 +0000

    Feature: Add error hook for App and Menu

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Cart from "@/features/cart/Cart";
 import CreateOrder from "@/features/order/CreateOrder";
 import Order from "@/features/order/Order";
 import AppLayout from "@/ui/AppLayout";
+import Error from "@/ui/Error";
 
 import { loader as menuLoader } from "@/services/constants";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -21,6 +23,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
